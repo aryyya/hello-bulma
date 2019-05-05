9f4e12c4076ee381d8a02d7f6d0a1f2ad86c76cf
aryyya <aryyya.xyz@gmail.com>
2019-05-05 12:23:48 -0700

    Add Bulma and Font Awesome dependencies.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './App.css'
 
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'bulma'
+
 function App() {
   return (
     <div>
-      Hello, Bulma!
+      <section className="hero is-fullheight">
+        <div className="hero-body">
+          Hello, Bulma!
+        </div>
+      </section>
     </div>
   )
 }
